7cf50f379632ac86580c9bde907e317df9c6a7d7
Jay Vaughan <seclorum@mac.com>
2024-03-27 23:58:48 +0100

    add key defs, hire hacking mode

--- toy.c
+++ toy.c
@@ -22,6 +22,7 @@ void Synth();
 #define POS_END   ((unsigned int)0xFFFF)
 #define POS_INITIAL_POSITION ((unsigned int)0xFAA7)
 #define POS_KEYCLICK1 ((unsigned int)0xFB32)
+#define HIRES_START   ((unsigned int)0xA000)
 
 extern unsigned char currentSound[];
 extern unsigned char BootSound[];
@@ -34,11 +35,28 @@ extern unsigned char BootSound[];
 #define PITCH_WORD(x) ((unsigned int)currentSound[x] + ((unsigned int)currentSound[x+1] << 8))
 #define CHAN_PITCH(c) (PITCH_WORD(c))
 
+#define APP_SOUND_ONOFF		'H'
+#define APP_POS_POSITIVE	'J'
+#define APP_POS_MINUS		'K'
+#define APP_RESET			'R'
+#define APP_RESET_POS		'.'
+#define APP_POSITION		'P'
+#define APP_HELP			'?'
+#define APP_HIRES			'G'
+#define APP_MEMHAK 			'M'
+#define APP_QUIT 			'Q'
+
+
+// from picture.s
+extern unsigned char OverlayLabel[];
+
 
 void printHelp()
 {
-	cls(); paper(0); ink(5);
-    printf("\nsoundtoy::\nA toy for exploring Oric ROM sounds.\n");
+	cls();
+	paper(0);
+	ink(5);
+	printf("\nsoundtoy::\nA toy for exploring Oric ROM sounds.\n");
 	printf("(by seclorum)\n\n");
 	printf("Key: Function:\n\n");
 	printf(" ?   Display this help.\n");
@@ -47,27 +65,32 @@ void printHelp()
 	printf(" J   Increment the \"PRESET pointer\".\n");
 	printf(" K   Decrement the \"PRESET pointer\".\n");
 	printf(" R   Reset to the \"bootup sound\".\n");
-	printf(" . 	 Reset to the \"PING\" sound.\n\n");
+	printf(" . 	 Reset to the \"PING\" sound.\n");
+	printf(" G 	 HIRES mode.\n");
 	printf(" Q	 Quit from soundtoy.\n\n");
 	printf("Any other key will trigger the synth.\n\n");
 	printf("Explore the Oric ROM for more sounds!\n");
 }
 
-char *charHexStr(unsigned char value) {
+char *charHexStr(unsigned char value)
+{
 	static char hexString[3];
-    static const char hexLookup[] = "0123456789ABCDEF";
-    hexString[0] = hexLookup[(value >> 4) & 0x0F];
-    hexString[1] = hexLookup[value & 0x0F];
-    hexString[2] = '\0';
-    return hexString;
+	static const char hexLookup[] = "0123456789ABCDEF";
+	hexString[0] = hexLookup[(value >> 4) & 0x0F];
+	hexString[1] = hexLookup[value & 0x0F];
+	hexString[2] = '\0';
+	return hexString;
 }
 
 void hexDump(unsigned char *addr, int length)
 {
-	int cnt=0;
-	int i=0;
-	for (i=0;i<length;i++) {
-		if (cnt++>7) { cnt = 0; printf("\n"); }
+	int cnt = 0;
+	int i = 0;
+	for (i = 0; i < length; i++) {
+		if (cnt++ > 7) {
+			cnt = 0;
+			printf("\n");
+		}
 		printf(" %s", charHexStr(addr[i]));
 		// if (i>13) printf(":"); else printf(" ");
 	}
@@ -77,19 +100,19 @@ void hexDump(unsigned char *addr, int length)
 loadTable(unsigned int address)
 {
 	currentSound[0] = peek(address);
-	currentSound[1] = peek(address+1);
-	currentSound[2] = peek(address+2);
-	currentSound[3] = peek(address+3);
-	currentSound[4] = peek(address+4);
-	currentSound[5] = peek(address+5);
-	currentSound[6] = peek(address+6);
-	currentSound[7] = peek(address+7);
-	currentSound[8] = peek(address+8);
-	currentSound[9] = peek(address+9);
-	currentSound[10] = peek(address+10);
-	currentSound[11] = peek(address+11);
-	currentSound[12] = peek(address+12);
-	currentSound[13] = peek(address+13);
+	currentSound[1] = peek(address + 1);
+	currentSound[2] = peek(address + 2);
+	currentSound[3] = peek(address + 3);
+	currentSound[4] = peek(address + 4);
+	currentSound[5] = peek(address + 5);
+	currentSound[6] = peek(address + 6);
+	currentSound[7] = peek(address + 7);
+	currentSound[8] = peek(address + 8);
+	currentSound[9] = peek(address + 9);
+	currentSound[10] = peek(address + 10);
+	currentSound[11] = peek(address + 11);
+	currentSound[12] = peek(address + 12);
+	currentSound[13] = peek(address + 13);
 }
 
 dumpPitch(unsigned char channel)
@@ -97,71 +120,142 @@ dumpPitch(unsigned char channel)
 	printf("[%d][p:%x]\n", CHAN_INDEX(channel), CHAN_PITCH(channel));
 }
 
-void main()
-{   
-	unsigned char k;		// key pressed
-	unsigned int p;			// position
-	unsigned int i; 		// input
+void SynthZP(unsigned char HI, unsigned char LO)
+{
+	poke(0x14, HI);
+	poke(0x15, LO);
+	call(0xF590);
+}
 
-	setflags(SCREEN+NOKEYCLICK);
+void main()
+{
+	unsigned char k;			// key pressed
+	unsigned int position;		// position
+	unsigned int i;				// input
+	unsigned char r;
+	unsigned char smode;
+	unsigned int *src, *dst;
+	int j;
+	int hires_mode = 0;
+
+	setflags(SCREEN + NOKEYCLICK);
 
 	printHelp();
 	Synth();
 
-	p=POS_INITIAL_POSITION; // start of ROM
-	
-	while((k=get())!='Q') {
+	position = POS_INITIAL_POSITION;	// start of ROM
+   	*src = HIRES_START;
+	*dst = HIRES_START;
 
-		if(k=='J') {
-			if (p++ >= POS_END) { p=POS_START; } 
+	while ((k = get()) != APP_QUIT) {
+
+		if (k == APP_SOUND_ONOFF) {
+			smode = !smode;
 		}
 		else
-		if(k=='K') {
-			if (p-- < POS_START) { p=POS_END; } 
-		}
-		else
-		if(k=='R') {
+		if (k == APP_POS_POSITIVE) {
+			if (position++ >= POS_END) {
+				position = POS_START;
+			}
+		} else if (k == APP_POS_MINUS) {
+			if (position-- < POS_START) {
+				position = POS_END;
+			}
+		} else if (k == APP_RESET) {
 			printf("reset!\n");
-			p=(unsigned int)&BootSound;
+			position = (unsigned int) &BootSound;
 			//p=POS_KEYCLICK1;
-		}
-		else
-		if(k=='.') {
+		} else if (k == APP_RESET_POS) {
 			printf(".\n");
-			p=POS_INITIAL_POSITION;
-		}
-		else
-		if(k=='P') {
+			position = POS_INITIAL_POSITION;
+		} else if (k == APP_POSITION) {
 			unsigned int v;
 			printf("P:");
 			scanf("%x", &v);
-			p=v;
-			printf("\nv:%x p:%x", v, p);
-		}
-		else 
-		if(k=='?') {
+			position = v;
+			printf("\nv:%x p:%x", v, position);
+		} else if (k == APP_HELP) {
 			printHelp();
+		} else if (k == APP_HIRES) {
+			if (hires_mode == 1) {
+				hires_mode = 0;
+				text();
+				continue;
+			}
+
+			hires_mode = 1;
+			position = HIRES_START;
+
+			hires();
+//          memcpy((unsigned char*)0xa000, OverlayLabel, 8000);
+
+			j = HIRES_START;
+
+			do {
+			//for (j = HIRES_START; j <= 0xBF3F; j++) {
+
+				if (j % 24) {
+					srandom(peek(0x276));
+				}
+/*
+				do {
+					r=random(1) % 255;
+				} while  ( ( (r>>3) == 1) || ( (r>>3) == 3) );
+*/
+				do {
+					r = random(1) % 255;
+					// r=peek(position);
+				} while
+					(((r & 0x78) == 0x08 || (r & 0x78) == 0x18) ||
+					 ((r & 0x78) == 0x88 || (r & 0x78) == 0x98));
+
+				poke(j, r);
+
+			} while (++j <= 0xBF3F);
+
 		}
+ 		if (k == APP_MEMHAK) {
+			if (hires_mode == 1) {
 
+			}
+		}
+ 
 		{
-			loadTable(p);
+			loadTable(position);
 
-			printf("\nP:%x\n", p);
-			hexDump((unsigned char *)p, 16);
+			printf("\nP:%x\n", position);
+			hexDump((unsigned char *) position, 16);
 
-#if 0
-			// disabled for now, will be the subject of more work in the near future
 			dumpPitch(CHAN_A);
 			dumpPitch(CHAN_B);
 			dumpPitch(CHAN_C);
-#endif
 
-			Synth();
+			if (smode) {
+				Synth();
+			}
+			else {
+				unsigned char H;
+				unsigned char L;
+				L = position << 8;
+				H = position &= 0xFF00;
+				printf("\nZP:%x %x %x\n", position, H, L);
+
+				SynthZP(H, L);
+			}
+
+
+			if (hires_mode == 1) {
+				int oldval = peek(position);
+				oldval = oldval ^= 128;
+				poke(position, oldval);
+			}
+
+
 		}
 
 	}
 
-	loadTable((unsigned int)&BootSound);
+	loadTable((unsigned int) &BootSound);
 
 	Synth();
 }
