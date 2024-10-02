581b503e6df06e49f6188bcde9f45f23fa09a995
Jay Vaughan <seclorum@mac.com>
2024-10-02 23:16:55 +0200

    multi-cell calls

--- cgen.c
+++ cgen.c
@@ -7,103 +7,163 @@
 #define ROW_SIZE 40
 #define BLOCK_HEIGHT 8
 
+#define NUM_CB_CELLS 4
 
-// Predefined opcode block (LDA, STA sequence for 8 rows)
-unsigned char pregenCodeBlock[24][51] = {
-    {0xA2, 0x00,             // LDX #$00 (initialize X register)
-    0xBD, 0x00, 0x00,       // LDA $0000,X (placeholder for source address)
-    0x9D, 0x00, 0x00,       // STA $0000,X (placeholder for destination address)
-    0xBD, 0x00, 0x00,       // LDA $0000,X (row 2)
-    0x9D, 0x00, 0x00,       // STA $0000,X
-    0xBD, 0x00, 0x00,       // LDA $0000,X (row 3)
-    0x9D, 0x00, 0x00,       // STA $0000,X
-    0xBD, 0x00, 0x00,       // LDA $0000,X (row 4)
-    0x9D, 0x00, 0x00,       // STA $0000,X
-    0xBD, 0x00, 0x00,       // LDA $0000,X (row 5)
-    0x9D, 0x00, 0x00,       // STA $0000,X
-    0xBD, 0x00, 0x00,       // LDA $0000,X (row 6)
-    0x9D, 0x00, 0x00,       // STA $0000,X
-    0xBD, 0x00, 0x00,       // LDA $0000,X (row 7)
-    0x9D, 0x00, 0x00,       // STA $0000,X
-    0xBD, 0x00, 0x00,       // LDA $0000,X (row 8)
-    0x9D, 0x00, 0x00,       // STA $0000,X
-    0x60},                  // RTS (return from subroutine)
-};   
+#define STOY1 0
+#define STOY2 1
+#define STOY3 2
+#define STOY4 3
+
+// Predefined op code block (LDA, STA sequence for 8 rows)
+unsigned char cellOpArray[NUM_CB_CELLS][51] = {
+
+  {
+   0xA2, 0x00,			// LDX #$00 (initialize X register)
+   0xBD, 0x00, 0x00,		// LDA $0000,X (placeholder for source address)
+   0x9D, 0x00, 0x00,		// STA $0000,X (placeholder for destination address)
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 2)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 3)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 4)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 5)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 6)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 7)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 8)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xEA},			// NOP ( RTS at last ll ]
+
+  {
+   0xA2, 0x00,			// LDX #$00 (initialize X register)
+   0xBD, 0x00, 0x00,		// LDA $0000,X (placeholder for source address)
+   0x9D, 0x00, 0x00,		// STA $0000,X (placeholder for destination address)
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 2)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 3)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 4)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 5)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 6)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 7)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 8)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xEA},			// NOP ( RTS at last ll ]
+
+  {
+   0xA2, 0x00,			// LDX #$00 (initialize X register)
+   0xBD, 0x00, 0x00,		// LDA $0000,X (placeholder for source address)
+   0x9D, 0x00, 0x00,		// STA $0000,X (placeholder for destination address)
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 2)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 3)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 4)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 5)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 6)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 7)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 8)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xEA},			// NOP ( RTS at last ll ]
+
+  {
+   0xA2, 0x00,			// LDX #$00 (initialize X register)
+   0xBD, 0x00, 0x00,		// LDA $0000,X (placeholder for source address)
+   0x9D, 0x00, 0x00,		// STA $0000,X (placeholder for destination address)
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 2)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 3)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 4)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 5)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 6)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 7)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0xBD, 0x00, 0x00,		// LDA $0000,X (row 8)
+   0x9D, 0x00, 0x00,		// STA $0000,X
+   0x60},			// RTS (return from subroutine)
+
+};
 
 // Function to modify the block with correct addresses based on cell index
-void genCellCopy(int sourceX, int sourceY, int destX, int destY) {
-    unsigned int source_base = SOURCE_BUFFER_START + (sourceY * ROW_SIZE * BLOCK_HEIGHT) + (sourceX * 1);
-    unsigned int dest_base = DESTINATION_BUFFER_START + (destY * ROW_SIZE * BLOCK_HEIGHT) + (destX * 1);
-	unsigned int source_addr;
-	unsigned int dest_addr;
+void
+genCellCopy (int cell, int sourceX, int sourceY, int destX, int destY)
+{
+  unsigned int source_base =
+    SOURCE_BUFFER_START + (sourceY * ROW_SIZE * BLOCK_HEIGHT) + (sourceX * 1);
+  unsigned int dest_base =
+    DESTINATION_BUFFER_START + (destY * ROW_SIZE * BLOCK_HEIGHT) +
+    (destX * 1);
+  unsigned int source_addr;
+  unsigned int dest_addr;
 
-    // Modify the source and destination addresses for each row (8 rows)
-	int pos;
-    for (pos = 0; pos < BLOCK_HEIGHT; pos++) {
-        // Source address modification
-        source_addr = source_base + (pos * ROW_SIZE);
-        pregenCodeBlock[0][3 + pos * 6] = source_addr & 0xFF;       // Low byte
-        pregenCodeBlock[0][4 + pos * 6] = (source_addr >> 8) & 0xFF; // High byte
-
-        // Destination address modification
-        dest_addr = dest_base + (pos * ROW_SIZE);
-        pregenCodeBlock[0][6 + pos * 6] = dest_addr & 0xFF;         // Low byte
-        pregenCodeBlock[0][7 + pos * 6] = (dest_addr >> 8) & 0xFF;  // High byte
+  if (cell >= NUM_CB_CELLS)
+    {
     }
-}
+  else
+    {
+      // Modify the source and destination addresses for each row (8 rows)
+      int pos;
+      for (pos = 0; pos < BLOCK_HEIGHT; pos++)
+	{
+	  // Source address modification
+	  source_addr = source_base + (pos * ROW_SIZE);
+	  cellOpArray[cell][3 + pos * 6] = source_addr & 0xFF;	// Low byte
+	  cellOpArray[cell][4 + pos * 6] = (source_addr >> 8) & 0xFF;	// High byte
 
-int cgen_test() {
-	int pos;
-	int level;
-	static int rando;
-	int key;
-
-	// simple example
-
-//	while ((key = get()) != 'Q') {
-	level=3;
-	while (1) {
-			rando++; if (rando>20) rando = 0;
-			genCellCopy(rando, 0, rando, 0); call(&pregenCodeBlock[0]);
-
-		for (pos=0;pos<24;pos++) {
-   	 		genCellCopy(0+rando, 0, pos, level); call(&pregenCodeBlock);
-    		genCellCopy(1+rando, 0, pos+1, level++); call(&pregenCodeBlock);
-    		genCellCopy(2+rando, 0, pos+2, level++); call(&pregenCodeBlock);
-    		genCellCopy(3+rando, 0, pos+3, level); call(&pregenCodeBlock);
-			level = 5;
-    		genCellCopy(23-rando, 0, 	pos+1, level++); call(&pregenCodeBlock);
-			genCellCopy(0, 20, 		 	pos, level - 1); call(&pregenCodeBlock);
-
-    		genCellCopy(22-rando-1, 0, pos+2, level++); call(&pregenCodeBlock);
-			genCellCopy(0, 20,		 	pos+1, level - 1); call(&pregenCodeBlock);
-
-
-    		genCellCopy(21-rando-2, 0, pos+3, level++); call(&pregenCodeBlock);
-			genCellCopy(0, 20,		 	pos+2, level - 1); call(&pregenCodeBlock);
-
-    		genCellCopy(20-rando-3, 0, pos+4, level++); call(&pregenCodeBlock);
-			genCellCopy(0, 20,		 	pos+3, level - 1); call(&pregenCodeBlock);
-
-   	 		genCellCopy(6+rando, 0, 1, 1); call(&pregenCodeBlock);
-		}
+	  // Destination address modification
+	  dest_addr = dest_base + (pos * ROW_SIZE);
+	  cellOpArray[cell][6 + pos * 6] = dest_addr & 0xFF;	// Low byte
+	  cellOpArray[cell][7 + pos * 6] = (dest_addr >> 8) & 0xFF;	// High byte
 	}
- 
-
-  //  genCellCopy(2, 1, 7, 7);
-   // call(&pregenCodeBlock);
-
-#if 0
-    // Example output of the modified block
-    print("Modified block for copying 8x8 cell:\n");
-    for (pos = 0; pos < sizeof(pregenCodeBlock); pos++) {
-        printf("%02X ", pregenCodeBlock[pos]);
-        if ((pos + 1) % 8 == 0) printf("\n");  // Format in rows
     }
-#endif
-
-    // At this point, you would jump to the `pregenCodeBlock` to execute it.
-
-    return 0;
 }
 
+int
+generateCells ()
+{
+  static int pos;
+  static int level;
+  static int rando;
+
+  int key;
+
+  // simple example
+  level = 2;
+  rando = 3;
+  while (1)
+    {
+      rando++;
+      if (rando > 24)
+	rando = 0;
+
+
+      genCellCopy (STOY1, rando + 1, 0, pos++, 0);
+      genCellCopy (STOY2, rando + 2, 0, pos++, level++);
+      genCellCopy (STOY3, rando + 3, 0, pos++, level++);
+      genCellCopy (STOY4, rando + 4, 0, pos++, level++);
+
+      if (level > 10)
+	level = 5;
+      if (pos > 20)
+	pos = 4;
+
+      call (&cellOpArray[0]);
+    }
+
+  return;
+}
--- toy.c
+++ toy.c
@@ -16,7 +16,7 @@
  */
 #include <lib.h>
 
-void Synth();
+void Synth ();
 
 #define POS_START 		((unsigned int)0x0000)
 #define POS_END   		((unsigned int)0xFFFF)
@@ -51,242 +51,282 @@ extern unsigned char BootSound[];
 extern unsigned char OverlayLabel[];
 
 
-void printHelp()
+void
+printHelp ()
 {
-	cls();
-	paper(0);
-	ink(5);
-	printf("\nsoundtoy::\nA toy for exploring Oric ROM sounds.\n");
-	printf("(by seclorum)\n\n");
-	printf("Key: Function:\n\n");
-	printf(" ?   Display this help.\n");
-	printf(" P   Set \"PRESET pointer\"\n     : enter HEX address.\n");
-	printf(" 	   e.g. FAA7, FABD, FAD3, FB10..\n");
-	printf(" J   Increment the \"PRESET pointer\".\n");
-	printf(" K   Decrement the \"PRESET pointer\".\n");
-	printf(" R   Reset to the \"bootup sound\".\n");
-	printf(" . 	 Reset to the \"PING\" sound.\n");
-	printf(" G 	 HIRES mode.\n");
-	printf(" Q	 Quit from soundtoy.\n\n");
-	printf("Any other key will trigger the synth.\n\n");
-	printf("Explore the Oric ROM for more sounds!\n");
+  cls ();
+  paper (0);
+  ink (5);
+  printf ("\nsoundtoy::\nA toy for exploring Oric ROM sounds.\n");
+  printf ("(by seclorum)\n\n");
+  printf ("Key: Function:\n\n");
+  printf (" ?   Display this help.\n");
+  printf (" P   Set \"PRESET pointer\"\n     : enter HEX address.\n");
+  printf (" 	   e.g. FAA7, FABD, FAD3, FB10..\n");
+  printf (" J   Increment the \"PRESET pointer\".\n");
+  printf (" K   Decrement the \"PRESET pointer\".\n");
+  printf (" R   Reset to the \"bootup sound\".\n");
+  printf (" . 	 Reset to the \"PING\" sound.\n");
+  printf (" G 	 HIRES mode.\n");
+  printf (" Q	 Quit from soundtoy.\n\n");
+  printf ("Any other key will trigger the synth.\n\n");
+  printf ("Explore the Oric ROM for more sounds!\n");
 }
 
-char *charHexStr(unsigned char value)
+char *
+charHexStr (unsigned char value)
 {
-	static char hexString[3];
-	static const char hexLookup[] = "0123456789ABCDEF";
-	hexString[0] = hexLookup[(value >> 4) & 0x0F];
-	hexString[1] = hexLookup[value & 0x0F];
-	hexString[2] = '\0';
-	return hexString;
+  static char hexString[3];
+  static const char hexLookup[] = "0123456789ABCDEF";
+  hexString[0] = hexLookup[(value >> 4) & 0x0F];
+  hexString[1] = hexLookup[value & 0x0F];
+  hexString[2] = '\0';
+  return hexString;
 }
 
-void hexDump(unsigned char *addr, int length)
+void
+hexDump (unsigned char *addr, int length)
 {
-	int cnt = 0;
-	int i = 0;
-	for (i = 0; i < length; i++) {
-		if (cnt++ > 7) {
-			cnt = 0;
-			printf("\n");
-		}
-		printf(" %s", charHexStr(addr[i]));
-		// if (i>13) printf(":"); else printf(" ");
+  int cnt = 0;
+  int i = 0;
+  for (i = 0; i < length; i++)
+    {
+      if (cnt++ > 7)
+	{
+	  cnt = 0;
+	  printf ("\n");
 	}
-	printf("\n");
+      printf (" %s", charHexStr (addr[i]));
+      // if (i>13) printf(":"); else printf(" ");
+    }
+  printf ("\n");
 }
 
-loadTable(unsigned int address)
+loadTable (unsigned int address)
 {
-	currentSound[0] = peek(address);
-	currentSound[1] = peek(address + 1);
-	currentSound[2] = peek(address + 2);
-	currentSound[3] = peek(address + 3);
-	currentSound[4] = peek(address + 4);
-	currentSound[5] = peek(address + 5);
-	currentSound[6] = peek(address + 6);
-	currentSound[7] = peek(address + 7);
-	currentSound[8] = peek(address + 8);
-	currentSound[9] = peek(address + 9);
-	currentSound[10] = peek(address + 10);
-	currentSound[11] = peek(address + 11);
-	currentSound[12] = peek(address + 12);
-	currentSound[13] = peek(address + 13);
+  currentSound[0] = peek (address);
+  currentSound[1] = peek (address + 1);
+  currentSound[2] = peek (address + 2);
+  currentSound[3] = peek (address + 3);
+  currentSound[4] = peek (address + 4);
+  currentSound[5] = peek (address + 5);
+  currentSound[6] = peek (address + 6);
+  currentSound[7] = peek (address + 7);
+  currentSound[8] = peek (address + 8);
+  currentSound[9] = peek (address + 9);
+  currentSound[10] = peek (address + 10);
+  currentSound[11] = peek (address + 11);
+  currentSound[12] = peek (address + 12);
+  currentSound[13] = peek (address + 13);
 }
 
-dumpPitch(unsigned char channel)
+dumpPitch (unsigned char channel)
 {
-	printf("[%d][p:%x]\n", CHAN_INDEX(channel), CHAN_PITCH(channel));
+  printf ("[%d][p:%x]\n", CHAN_INDEX (channel), CHAN_PITCH (channel));
 }
 
-void SynthZP(unsigned char HI, unsigned char LO)
+void
+SynthZP (unsigned char HI, unsigned char LO)
 {
-	poke(0x14, HI);
-	poke(0x15, LO);
-	call(0xF590);
+  poke (0x14, HI);
+  poke (0x15, LO);
+  call (0xF590);
 }
 
-void gen_rnd_colors(){
-
-	int j;
-	int k;
-
-	volatile char r;
-	char s;
-
-	// s = peek(0x276);
-	// seed_lfsr(s);
-
-	j = (unsigned int)HIRES_START;
-	k = (unsigned int)HIRES_START + 320*3;
-//	k = (unsigned int)HIRES_END;
-
-	do {
-
-		// r = 16+((qrandomJ(peek(0x276)) % 255) & 7);
-
-		// // Somewhat slow C-based implementation with assembly RNG - works
-		do {
-			r  = qrandomJ(peek(0x276)) % 255;
-		} while (((r & 0x78) == 0x08 || (r & 0x78) == 0x18) || ((r & 0x78) == 0x88 || (r & 0x78) == 0x98));
-
-		// r  = qrandomJ(peek(0x276)) % 255;
-
-		// Linear-feedback shift register method
-		// r = lfsr_random();
-
-		// table-based with 
-		// r = fastbloop();
-
-		// Chema's randgen:
-		// r == randgen();
-
-		poke(j, r);
-		// printf("j: %x r: %x\n", j, r);
-
-	} while (++j < k);
-
-}
-
-void main()
+void
+gen_rnd_colors ()
 {
-	unsigned char k;			// key pressed
-	unsigned int position;		// position
-	unsigned int i;				// input
-	unsigned char r;
-	unsigned char smode;
-	unsigned char *src, *dst;
-	int j;
-	int m;
-	int hires_mode = 0;
-	int bloop_mode = 0;
 
-	setflags(SCREEN + NOKEYCLICK);
+  int j;
+  int k;
 
-	printHelp();
-	Synth();
+  volatile char r;
+  char s;
 
-	position = POS_INITIAL_POSITION;	// start of ROM
-	src = HIRES_START;
-	dst = HIRES_START+40;
+  // s = peek(0x276);
+  // seed_lfsr(s);
 
-	while ((k = get()) != APP_QUIT) {
+  j = (unsigned int) HIRES_START;
+  k = (unsigned int) HIRES_START + 320 * 3;
+//      k = (unsigned int)HIRES_END;
 
-		if (k == APP_SOUND_ONOFF) {
-			smode = !smode;
-		}
-		else if (k == APP_POS_POSITIVE) {
-				if (position++ >= POS_END) {
-					position = POS_START;
-				}
-			} 
-		else if (k == APP_POS_MINUS) {
-				if (position-- < POS_START) {
-					position = POS_END;
-				}
-		} else if (k == APP_RESET) {
-				printf("reset!\n");
-				position = (unsigned int) &BootSound;
-			//p=POS_KEYCLICK1;
-		} else if (k == APP_RESET_POS) {
-				printf(".\n");
-				position = POS_INITIAL_POSITION;
-		} else if (k == APP_POSITION) {
-				unsigned int v;
-				printf("P:");
-				scanf("%x", &v);
-				position = v;
-				printf("\nv:%x p:%x", v, position);
-		} else if (k == APP_HELP) {
-				printHelp();
-		} else if (k == APP_HIRES) {
-				hires();
-				// memcpy((unsigned char*)0xa000, OverlayLabel, 8000);
-				hires_mode++; if (hires_mode >= 3) hires_mode = 0;
-				gen_rnd_colors();
-				cgen_test();
+  do
+    {
 
-				// randcolorgen();
-				// randcogtab();
+      // r = 16+((qrandomJ(peek(0x276)) % 255) & 7);
 
-				// if (hires_mode==0){
-				// 	text();
-				// 	continue;
-				// }
-
-				// if (hires_mode == 1) {
-				// 	spamit();
-				// }
-				// if (hires_mode == 2) {
-				// 	gen_rnd_colors();
-				// }
-
-				// if (hires_mode == 3) {
-				// 	int oldval = peek(position);
-				// 	oldval = oldval ^= 128;
-				// 	poke(position, oldval);
-			} else if (k == APP_MEMHAK) {
-				// dst = (void *)(HIRES_START + (8*40);
-				// for (src = (void *)HIRES_START; 
-				// 	src < HIRES_END - (8*40) - 4; 
-				// 	src += bloop_mode, dst += bloop_mode) {
-
-				// 	memcpy(src+(1*40), dst+(1*40), bloop_mode);
-				// 	memcpy(src+(2*40), dst+(2*40), bloop_mode);
-				// 	memcpy(src+(3*40), dst+(3*40), bloop_mode);
-				// 	memcpy(src+(4*40), dst+(4*40), bloop_mode);
-				// 	memcpy(src+(5*40), dst+(5*40), bloop_mode);
-				// 	memcpy(src+(6*40), dst+(6*40), bloop_mode);
-				// 	memcpy(src+(7*40), dst+(7*40), bloop_mode);
-				// 	memcpy(src+(8*40), dst+(8*40), bloop_mode);
-			}
-
-			loadTable(position);
-
-			printf("\nP:%x\n", position);
-			hexDump((unsigned char *) position, 16);
-
-			dumpPitch(CHAN_A);
-			dumpPitch(CHAN_B);
-			dumpPitch(CHAN_C);
-
-			if (smode) {
-				Synth();
-			}
-			else {
-				unsigned char H;
-				unsigned char L;
-				L = position << 8;
-				H = position &= 0xFF00;
-				//printf("\nZP:%x %x %x\n", position, H, L);
-
-				SynthZP(H, L);
-			}
-
-		}
-
-		loadTable((unsigned int) &BootSound);
-
-		Synth();
+      // // Somewhat slow C-based implementation with assembly RNG - works
+      do
+	{
+	  r = qrandomJ (peek (0x276)) % 255;
 	}
+      while (((r & 0x78) == 0x08 || (r & 0x78) == 0x18)
+	     || ((r & 0x78) == 0x88 || (r & 0x78) == 0x98));
+
+      // r  = qrandomJ(peek(0x276)) % 255;
+
+      // Linear-feedback shift register method
+      // r = lfsr_random();
+
+      // table-based with 
+      // r = fastbloop();
+
+      // Chema's randgen:
+      // r == randgen();
+
+      poke (j, r);
+      // printf("j: %x r: %x\n", j, r);
+
+    }
+  while (j++ < k);
+
+}
+
+void
+main ()
+{
+  unsigned char k;		// key pressed
+  unsigned int position;	// position
+  unsigned int i;		// input
+  unsigned char r;
+  unsigned char smode;
+  unsigned char *src, *dst;
+  int j;
+  int m;
+  int hires_mode = 0;
+  int bloop_mode = 0;
+
+  setflags (SCREEN + NOKEYCLICK);
+
+  printHelp ();
+  Synth ();
+
+  position = POS_INITIAL_POSITION;	// start of ROM
+  src = HIRES_START;
+  dst = HIRES_START + 40;
+
+  while ((k = get ()) != APP_QUIT)
+    {
+
+      if (k == APP_SOUND_ONOFF)
+	{
+	  smode = !smode;
+	}
+      else if (k == APP_POS_POSITIVE)
+	{
+	  if (position++ >= POS_END)
+	    {
+	      position = POS_START;
+	    }
+	}
+      else if (k == APP_POS_MINUS)
+	{
+	  if (position-- < POS_START)
+	    {
+	      position = POS_END;
+	    }
+	}
+      else if (k == APP_RESET)
+	{
+	  printf ("reset!\n");
+	  position = (unsigned int) &BootSound;
+	  //p=POS_KEYCLICK1;
+	}
+      else if (k == APP_RESET_POS)
+	{
+	  printf (".\n");
+	  position = POS_INITIAL_POSITION;
+	}
+      else if (k == APP_POSITION)
+	{
+	  unsigned int v;
+	  printf ("P:");
+	  scanf ("%x", &v);
+	  position = v;
+	  printf ("\nv:%x p:%x", v, position);
+	}
+      else if (k == APP_HELP)
+	{
+	  printHelp ();
+	}
+      else if (k == APP_HIRES)
+	{
+
+	  hires ();
+
+	  // memcpy((unsigned char*)0xa000, OverlayLabel, 8000);
+	  hires_mode++;
+	  if (hires_mode >= 3)
+	    hires_mode = 0;
+
+	  gen_rnd_colors ();
+
+	  position = generateCells ();
+
+	  // randcolorgen();
+	  // randcogtab();
+
+	  // if (hires_mode==0){
+	  //      text();
+	  //      continue;
+	  // }
+
+	  // if (hires_mode == 1) {
+	  //      spamit();
+	  // }
+	  // if (hires_mode == 2) {
+	  //      gen_rnd_colors();
+	  // }
+
+	  // if (hires_mode == 3) {
+	  //      int oldval = peek(position);
+	  //      oldval = oldval ^= 128;
+	  //      poke(position, oldval);
+	}
+      else if (k == APP_MEMHAK)
+	{
+	  // dst = (void *)(HIRES_START + (8*40);
+	  // for (src = (void *)HIRES_START; 
+	  //      src < HIRES_END - (8*40) - 4; 
+	  //      src += bloop_mode, dst += bloop_mode) {
+
+	  //      memcpy(src+(1*40), dst+(1*40), bloop_mode);
+	  //      memcpy(src+(2*40), dst+(2*40), bloop_mode);
+	  //      memcpy(src+(3*40), dst+(3*40), bloop_mode);
+	  //      memcpy(src+(4*40), dst+(4*40), bloop_mode);
+	  //      memcpy(src+(5*40), dst+(5*40), bloop_mode);
+	  //      memcpy(src+(6*40), dst+(6*40), bloop_mode);
+	  //      memcpy(src+(7*40), dst+(7*40), bloop_mode);
+	  //      memcpy(src+(8*40), dst+(8*40), bloop_mode);
+	}
+
+      loadTable (position);
+
+      printf ("\nP:%x\n", position);
+      hexDump ((unsigned char *) position, 16);
+
+      dumpPitch (CHAN_A);
+      dumpPitch (CHAN_B);
+      dumpPitch (CHAN_C);
+
+      if (smode)
+	{
+	  Synth ();
+	}
+      else
+	{
+	  unsigned char H;
+	  unsigned char L;
+	  L = position << 8;
+	  H = position &= 0xFF00;
+	  //printf("\nZP:%x %x %x\n", position, H, L);
+
+	  SynthZP (H, L);
+	}
+
+    }
+
+  loadTable ((unsigned int) &BootSound);
+
+  Synth ();
+}
